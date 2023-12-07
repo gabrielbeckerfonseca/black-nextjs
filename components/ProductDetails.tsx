0afaedb7de60a9a695370fc4399a82f4b7b6f610
Gabriel Becker da Fonseca <gbeckerdafonseca@gmail.com>
2023-12-06 21:20:36 -0300

    Finalizando o site

--- components/ProductDetails.tsx
+++ components/ProductDetails.tsx
@@ -1,8 +1,9 @@
 import Image from "next/image";
-import React, { useState } from "react";
+import React, { useContext, useState } from "react";
 import { Button, Col, Row } from "reactstrap";
 import { ProductType } from "../services/products";
 import SuccessToast from "./SuccessToast";
+import { useCart } from "../hooks/useCart";
 
 type ProductDetailsProps = {
   product: ProductType
@@ -10,6 +11,7 @@ type ProductDetailsProps = {
 
 const ProductDetails: React.FC<ProductDetailsProps> = ({ product }) => {
   const [toastIsOpen, setToastIsOpen] = useState(false)
+  const { addProduct } = useCart()
 
   return (
     <Row>
@@ -39,6 +41,7 @@ const ProductDetails: React.FC<ProductDetailsProps> = ({ product }) => {
           className="my-3 pb-2"
           block
           onClick={() => {
+            addProduct(product);
             setToastIsOpen(true)
             setTimeout(() => setToastIsOpen(false), 1000 * 3)
         }}
